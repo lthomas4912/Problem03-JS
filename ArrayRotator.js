7c39ec088798968c7420903a55df17574d4de82d
leah4912 <leah29726@gmail.com>
2020-09-24 21:12:47 -0400

    passed test dos

--- ArrayRotator.js
+++ ArrayRotator.js
@@ -1,6 +1,12 @@
 class ArrayRotator {
     rotateLeft(arr, n){
-
+       
+    let i = 0;
+    while(i < n){
+        arr.unshift(arr.pop());
+        i++;
+       
+    }
         return arr;
     }
 }
